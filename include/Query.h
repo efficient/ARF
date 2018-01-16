c961823b2808dc606d8ff5f5829db655831b52f5
Huanchen Zhang <huanchenzhang.cs@gmail.com>
2018-01-16 13:32:11 -0500

    fast ARF

---
+++ include/Query.h
@@ -0,0 +1,14 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+#include "Util.h"
+
+class Query {
+ public:
+    struct Query_t {
+        uint64 left;
+        uint64 right;
+    };  // Query_t
+};
+
+#endif // Query_H
